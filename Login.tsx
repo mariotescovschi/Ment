b8e31a4618034144f81781171711e3972cfa413b
George Vasile <vasileirinel013@gmail.com>
2023-08-13 15:07:44 +0300

    numpad

--- Login.tsx
+++ Login.tsx
@@ -1,18 +1,68 @@
-import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+    ActivityIndicator,
+    Button,
+    KeyboardAvoidingView,
+    Pressable,
+    StyleSheet,
+    Text,
+    TextInput,
+    View
+} from 'react-native';
 import {ParamListBase, useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import React, {useState} from "react";
 import {FIREBASE_AUTH} from './FireBaseConfig';
-import {isLoading} from "expo-font";
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
+
 
 const Login = () => {
     const [phone, setPhone] = useState('');
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [password, setPassword] = useState('');
     const auth=FIREBASE_AUTH;
+
+    const signIn = async() =>{
+        setLoading(true);
+        try{
+            const response = await signInWithEmailAndPassword(auth, email, password);
+            console.log(response);
+        } catch(error){
+            console.log(error);
+            alert('SignIn Failed!');
+        } finally {
+            setLoading(false);
+        }
+    }
+    const signUp = async() =>{
+        setLoading(true);
+        try{
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(response);
+            alert('Check your emails');
+        } catch(error){
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return(
         <View style={styles.container}>
-            <TextInput style={styles.input} keyboardType={'phone-pad'} placeholder="Phone Number" autoCapitalize="none" onChangeText={(text) => setPhone(text)}></TextInput>
+            <KeyboardAvoidingView behavior = 'padding'>
+            <TextInput value={email} style={styles.input}  placeholder="Email" autoCapitalize="none" onChangeText={(text) => setEmail(text)}></TextInput>
+            <TextInput secureTextEntry={true} value={password} style={styles.input} autoCapitalize="none" placeholder='Password' onChangeText={(text) => setPassword(text)}></TextInput>
+            {loading ?
+                (<ActivityIndicator size="large" color="#0000ff"/>
+            ):(
+                <>
+                <Button title="Login" onPress={() => signIn()} />
+                <Button title="SignUp" onPress={() => signUp()} />
+                </>
+                )
+                }
+            </KeyboardAvoidingView>
         </View>
     );
 }
